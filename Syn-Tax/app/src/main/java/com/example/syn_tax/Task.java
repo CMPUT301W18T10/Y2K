a00f0c9d1f597e4880b73e730e0cc3176b28d976
aojetola <abdulaze@ualberta.ca>
2018-02-22 00:37:55 -0700

    started Test cases for task status

--- Syn-Tax/app/src/main/java/com/example/syn_tax/Task.java
+++ Syn-Tax/app/src/main/java/com/example/syn_tax/Task.java
@@ -1,10 +1,71 @@
 package com.example.syn_tax;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by Hamsemare on 2018-02-21.
  */
 
 public class Task {
+    private String TaskName;
+    private String TaskObjective;
+    private String TaskStatus;
+    private Date TaskDate;
+    private Double  TaskBid;
+    private ArrayList<Double> TaskBids = new ArrayList<Double>();
 
-    public Task(String title, String description, User user ){}
+
+    public Task(String Name, String Objective, String Status, Double Bid) {
+        this.TaskName = Name;
+        this.TaskObjective = Objective;
+        this.TaskStatus = Status;
+        this.TaskBid = Bid;
+        //this.TaskDate = Date;
+    }
+
+    public String getTaskName() {
+        return TaskName;
+    }
+
+    public void setTaskName(String taskName) {
+        TaskName = taskName;
+    }
+
+    public String getTaskObjective() {
+        return TaskObjective;
+    }
+
+    public void setTaskObjective(String taskObjective) {
+        TaskObjective = taskObjective;
+    }
+
+    public String getTaskStatus() {
+        return TaskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        TaskStatus = taskStatus;
+    }
+
+    public Date getTaskDate() {
+        return TaskDate;
+    }
+
+    public void setTaskDate(Date taskDate) {
+        TaskDate = taskDate;
+    }
+
+    public void AddTaskBids(double Bid){
+        TaskBid = Bid;
+        TaskBids.add(0,TaskBid);
+    }
+
+    public Double getTaskBid(int count){
+
+        return TaskBids.get(0);
+
+    }
+
 }
---
+++ Syn-Tax/app/src/test/java/com/example/syn_tax/TestTaskStatus.java
@@ -0,0 +1,42 @@
+package com.example.syn_tax;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by abdulazeezojetola on 2018-02-21.
+ */
+
+public class TestTaskStatus extends ActivityInstrumentationTestCase2 {
+    public TestTaskStatus(){
+        super(Task.class);
+    }
+
+    public void TestStatus() {
+        Task task = new Task("Test", "Status Test", "Requested", 25.50);
+
+        // When a task is initialized it status is requested, so first, check to see if the status equals
+        // requested
+        assertEquals("Requested", task.getTaskStatus());
+
+        // Change task to assigned - this is after a bid has been accepted and assigned to a provider
+        // then check if the status has indeed changed
+        task.setTaskStatus("Assigned");
+        assertEquals("Assigned", task.getTaskStatus());
+
+        // change to completed when a task her been completed and no longer can be bidded on
+        // check if it has changed using assertEquals
+        task.setTaskStatus("Completed");
+        assertEquals("Completed", task.getTaskStatus());
+
+        // After a bid has been made on a task, the status changes
+        // using assertequal to check if the status is correct
+        task.setTaskStatus("Bidded");
+        assertEquals("Bidded", task.getTaskStatus());
+
+
+
+
+
+
+    }
+
+}
